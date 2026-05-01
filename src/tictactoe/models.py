1b018665bd8b57286cd34c9853af0b96f87bf2d9
Princeton Afeez <princetonafeez@gmail.com>
2026-04-30 19:57:43 -0700

    Immutable domain models for Tic-Tac-Toe

---
+++ src/tictactoe/models.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from enum import Enum
+from time import monotonic
+from typing import ClassVar
+
+from .exceptions import CellOccupiedError, InvalidBoardSizeError, OutOfBoundsError
+
